690b987d5593ab7b99c313a63965f6c648f40e1a
custer <custertian@gmail.com>
2017-03-24 18:42:02 +0800

    My resume commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,45 @@
+// $("#main").append("custer");
+// [string].replace([old],[new])
+
+// var email = "custer@udacity.com";
+
+// var newEmail = email.replace("udacity", "gmail");
+
+// console.log(email);
+// console.log(newEmail);
+
+// var awesomeThoughts = "My name is Custer and I am AWESOME!";
+
+// var funThougts = awesomeThoughts.replace("AWESOME!", "FUN");
+
+// $("#main").append(funThougts);
+
+// var formattedName = HTMLheaderName.replace("%data%", "Custer Tian");
+// $("#header").append(formattedName);
+
+// var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+// // $("#header").prepend(formattedRole);
+// $('#header').append(formattedRole);
+
+// var skills = ["awesomeness", "programming", "JavaScript", "Python"];
+// $("#main").append(skills);
+// $("#main").append(skills[0]);
+// $("#main").append(skills.length);
+
+// var bio = {
+//     "name" : "custer",
+//     "role" : "Web Developer",
+//     "contacts" : {
+//         "mobile" : "18801796642",
+//         "email" : "custertian@163.com",
+//         "github" : "custertian",
+//         "location" : "Shanghai"
+//     },
+//     "pictureURL" : "images/me.jpg",
+//     "welcomeMessage" : "Welcome to see my profile.",
+//     "skills" : ["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"]
+// };
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// $("#header").append(formattedName);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// $("#header").append(formattedRole);
